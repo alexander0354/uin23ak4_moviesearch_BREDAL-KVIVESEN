73b30634ec20a3c3e212b9d27dace174e9c3519a
alexander0354 <alebrekvi@gmail.com>
2023-03-30 13:56:07 +0200

    lagt til kommentarer

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,27 +2,31 @@ import React, { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import SearchBar from "./SearchBar";
 
+
+//komponent for å fetche filmer til nettsiden
+
 function MovieList() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    // "default search query"
+    // "default search query" for å hente 10 James Bond-filmer
     fetchMovies("James+Bond");
   }, []);
 
-  // Async - await syntax for å fetche filmers data fra APIet
+  // Async - await syntax for å hente filmdata fra APIet
   const fetchMovies = async (query) => {
     const response = await fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=ffceff3589088ca1ba677a8bfff757d8&query=${query}`
     );
     const data = await response.json();
-    // Fetcher filmer
-    setMovies(data.results);
+    // Henter kun de første 10 filmene fra svaret
+    setMovies(data.results.slice(0, 10));
   };
 
-  //Fetcher filmdata basert på søkeord fra brukeren 
+  // Håndterer søk fra brukeren
   const handleSearch = (searchWord) => {
-    fetchMovies(searchWord);
+    // Bruker søkeordet og legger til resten av query-variablene for å hente de første 10 resultatene
+    fetchMovies(`${searchWord}`);
   };
 
   return (
@@ -30,7 +34,7 @@ function MovieList() {
       {/*Importerer SearchBar til liste-komponentet */}
       <SearchBar onSearch={handleSearch} />
       <div className="movie-list">
-        {/*fetcher filmer basert på id*/}
+        {/*Lager en MovieCard-komponent for hver film*/}
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
@@ -38,4 +42,5 @@ function MovieList() {
     </div>
   );
 }
+
 export default MovieList;
